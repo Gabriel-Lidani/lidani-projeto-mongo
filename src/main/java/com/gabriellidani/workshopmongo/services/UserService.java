eb971e258b16c0c0d4cc0e55d98dd1b12beaef5a
Gabriel Lidani <gabriellidani15@gmail.com>
2026-05-14 12:53:56 -0300

    Insercaoo de usuario com POST

--- src/main/java/com/gabriellidani/workshopmongo/services/UserService.java
+++ src/main/java/com/gabriellidani/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriellidani.workshopmongo.domain.User;
+import com.gabriellidani.workshopmongo.dto.UserDTO;
 import com.gabriellidani.workshopmongo.repository.UserRepository;
 import com.gabriellidani.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
